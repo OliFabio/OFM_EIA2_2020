173c612468d006dea2f75410cf42810fb22f032c
OliFabio <64095018+OliFabio@users.noreply.github.com>
2020-10-08 13:10:21 +0200

    PromptBox

---
+++ L01_Übung_Popup/script.js
@@ -0,0 +1,6 @@
+"use strict";
+var person = prompt("Tell me your name");
+window.addEventListener("load", function () {
+    document.getElementById("answer").innerHTML = "Hello my friend " + person + ". How are you today?";
+});
+//# sourceMappingURL=script.js.map
---
+++ L01_Übung_Popup/script.ts
@@ -0,0 +1,4 @@
+var person: string = prompt("Tell me your name");
+
+window.addEventListener("load", function() {
+document.getElementById("answer").innerHTML = "Hello my friend " + person + ". How are you today?"});
